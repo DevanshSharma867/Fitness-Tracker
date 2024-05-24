1d63dfa6803c0d109a7682cf92fe4fe98c92a141
Devansh Sharma <sdevansh772@gmail.com>
2024-05-24 16:29:56 +0530

    First commit

---
+++ src/Components/FitnessTrackerApp.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+import 'C:\\Users\\sdeva\\.vscode\\Learning\\fitness-tracker-app\\src\\Styles\\FitnessTrackerApp.css';
+
+const FitnessTrackerApp = () => {
+  const [exercises, setExercises] = useState([]);
+  const [exercise, setExercise] = useState('');
+  const [duration, setDuration] = useState('');
+
+  const addExercise = () => {
+    if (exercise && duration) {
+      setExercises([...exercises, { exercise, duration }]);
+      setExercise('');
+      setDuration('');
+    }
+  };
+
+  return (
+    <div className="app">
+      <h1>Fitness Tracker</h1>
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Exercise"
+          value={exercise}
+          onChange={(e) => setExercise(e.target.value)}
+        />
+        <input
+          type="text"
+          placeholder="Duration (mins)"
+          value={duration}
+          onChange={(e) => setDuration(e.target.value)}
+        />
+        <button onClick={addExercise}>Add Exercise</button>
+      </div>
+      <div className="exercise-list">
+        {exercises.map((item, index) => (
+          <div key={index} className="exercise-item">
+            <span>{item.exercise}</span>
+            <span>{item.duration} mins</span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FitnessTrackerApp;
